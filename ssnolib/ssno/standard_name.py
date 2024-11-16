24a0fed37c22d844cfd989d2dc8aa72b16b6ece8
MatthiasProbst <matth.probst@gmail.com>
2024-11-16 09:46:20 +0100

    bugfix default field

--- ssnolib/ssno/standard_name.py
+++ ssnolib/ssno/standard_name.py
@@ -29,7 +29,7 @@ class StandardName(Concept):
     )
 
     standardName: str = Field(alias="standard_name")
-    unit: Union[str, HttpUrl]=None  # required!
+    unit: Union[str, HttpUrl]  # required!
     description: Union[str, List[str]] = None  # ssno:description
     standardNameTable: Union[str, Dataset, "StandardNameTable"] = Field(default=None, alias="standard_name_table")
     alias: Optional[Union["StandardName", HttpUrl, str]] = Field(default=None)
