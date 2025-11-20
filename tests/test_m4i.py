0daad703690fdb3f88788834430a798ef04591ca
MatthiasProbst <matth.probst@gmail.com>
2025-11-20 18:02:31 +0100

    upgrade to ontolutils 0.22.1

--- tests/test_m4i.py
+++ tests/test_m4i.py
@@ -1,5 +1,7 @@
 import unittest
 
+import pydantic
+
 from ssnolib import StandardName
 from ssnolib.m4i import NumericalVariable
 
@@ -7,16 +9,12 @@ from ssnolib.m4i import NumericalVariable
 class TestM4i(unittest.TestCase):
 
     def testNumericalVariableWithoutStandardName(self):
-        numerical_variable = NumericalVariable(
-            hasUnit='Unit',
-            hasNumericalValue=1.0,
-            hasMaximumValue=2.0,
-            hasVariableDescription='Variable description')
-        self.assertEqual(numerical_variable.hasUnit, 'Unit')
-        self.assertEqual(numerical_variable.hasNumericalValue, 1.0)
-        self.assertEqual(numerical_variable.hasMaximumValue, 2.0)
-        self.assertEqual(numerical_variable.hasVariableDescription, 'Variable description')
-        self.assertEqual(numerical_variable.hasStandardName, None)
+        with self.assertRaises(pydantic.ValidationError):
+                numerical_variable = NumericalVariable(
+                    hasUnit='Unit',
+                    hasNumericalValue=1.0,
+                    hasMaximumValue=2.0,
+                    hasVariableDescription='Variable description')
 
     def testNumericalVariableWithStandardName(self):
         numerical_variable = NumericalVariable(
@@ -28,7 +26,7 @@ class TestM4i(unittest.TestCase):
                 standardName='x_velocity',
                 unit='m/s')
         )
-        self.assertEqual(numerical_variable.hasUnit, 'mm/s')
+        self.assertEqual(numerical_variable.hasUnit, 'http://qudt.org/vocab/unit/MilliM-PER-SEC')
         self.assertEqual(numerical_variable.hasNumericalValue, 1.0)
         self.assertEqual(numerical_variable.hasMaximumValue, 2.0)
         self.assertEqual(numerical_variable.hasVariableDescription, 'Variable description')
